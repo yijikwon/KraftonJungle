1ed1f80e9872564322c459903d387a1da4fdb1fc
yijikwon <oiiziixo@gmail.com>
2026-09-07 10:52:50 +0900

    이진 검색 트리 문제풀이 과정

--- week3/01_basic/02_bst.py
+++ week3/01_basic/02_bst.py
@@ -51,12 +51,21 @@ def search_bst(root, target):
         True/False
     """
     # TODO: root가 None이면 False 반환
-    pass
-    
+    if root == None:
+
+        return False
     # TODO: 값을 찾으면 True 반환
+    elif root.value == target:
+
+        return True
     ## target이 작으면 왼쪽 서브트리에서 검색
+    elif root.value > target:
+
+        return search_bst(root.left, target)
     ## target이 크면 오른쪽 서브트리에서 검색
-    pass
+    else :
+
+        return search_bst(root.right, target)
 
 # 테스트 케이스
 if __name__ == "__main__":
